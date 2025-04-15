cbc655746d01b2f3ef844cea7ffa61cc4cef85ef
Pavlo Pototskyi <spirit.kherson@gmail.com>
2025-04-15 08:17:54 +0200

    A command to install os from Linux live cd

---
+++ run.sh
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -enable-kvm -m 16g -k en-us -cdrom proxmox-ve_8.4-1.iso -cpu host -drive file=/dev/sda,format=raw,media=disk,id=disk1  -vnc 127.0.0.1:1
