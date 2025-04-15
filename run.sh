abaebeec89d6d415da2e87b55a1ff97d27ec8f54
spiritks <54471095+spiritks@users.noreply.github.com>
2025-04-15 08:20:56 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -enable-kvm -m 16g -k en-us -cdrom proxmox-ve_8.4-1.iso -cpu host -drive file=/dev/sda,format=raw,media=disk,id=disk1  -vnc 127.0.0.1:1
+
+# ssh -L 8888:127.0.0.1:5901 rootyour_server_with_live_cd
